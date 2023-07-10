10d5a6c0cec7f4d27569f61c5e646b9598ba05b3
pronamix <139091238+pronamix@users.noreply.github.com>
2023-07-09 21:59:25 -0500

    Update Backup_Timestamp_fcn.sh

--- Backup_Timestamp_fcn.sh
+++ Backup_Timestamp_fcn.sh
@@ -4,7 +4,7 @@ DIR1=${1%/}/
 DIR2=${2%/}/
 DIR2_Date="${DIR2}"`date +%F_%H-%M-%S`/
 
-mkdir "${DIR2}" 2>/dev/null
-mkdir "${DIR2_Date}" 2>/dev/null
+mkdir "${DIR2}" >/dev/null
+mkdir "${DIR2_Date}" >/dev/null
 
 sudo rsync --partial --append-verify --recursive --relative "${DIR1}./" "${DIR2_Date}"
