9e9590c4a466218fd0ba2f501dcd0efd0486463e
pronamix <139091238+pronamix@users.noreply.github.com>
2023-07-09 22:29:25 -0500

    Create smb.sh

---
+++ smb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Install Samba
+sudo apt install -y samba samba-common-bin
+
+# Delete Existing smb.conf
+#sudo rm -f "/etc/samba/smb.conf"
+
+# Generate smb.conf
+sudo tee -a "/etc/samba/smb.conf" > /dev/null <<EOT
+[global]
+   unix extensions = NO
+   workgroup = WORKGROUP
+   map to guest = BAD USER
+   usershare allow guests = YES
+   store dos attributes = NO
+
+[root]
+   path = /
+   public = YES
+   read only = NO
+   writeable = YES
+   guest ok = YES
+   guest only = YES
+   browseable = YES
+   wide links = YES
+   hide dot files = NO
+   follow symlinks = YES
+   protocol = SMB3
+   force user = root
+   create mask = 0777
+   directory mask = 0777
+   force create mode = 0777
+   force directory mode = 0777
+EOT
+
+# Services
+sudo service smbd enable
+sudo service smbd reload
+sudo service smbd restart
+
+sudo service nmbd enable
+sudo service nmbd reload
+sudo service nmbd restart
