74d9260a40300e93980ad07fc17a8e2cd87a91ef
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-09-28 09:29:30 -0500

    Update and rename README.md to smb.sh

--- README.md
+++
@@ -1,1 +0,0 @@
-curl https://raw.githubusercontent.com/AlgoClaw/Bash/main/smb.sh --create-dirs -o /Docker/Scripts/smb.sh && sudo bash /Docker/Scripts/smb.sh
---
+++ smb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm sudo bash /Scripts/smb_temp.sh & wait
+
+curl https://raw.githubusercontent.com/AlgoClaw/Bash/main/smb_tmp.sh --create-dirs -o /Scripts/smb_tmp.sh & wait
+
+sudo bash /Scripts/smb_temp.sh
