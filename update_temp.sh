b0c42f7f75f4a24e0bb01e58d1f5b3edd3413cda
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-09-28 08:50:50 -0500

    Update and rename update.sh to update_temp.sh

--- update_temp.sh
+++ update_temp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo rm /etc/apt/apt.conf.d/20apt-esm-hook.conf & wait
+sudo apt install -y ubuntu-advantage-tools & wait
 sudo dpkg --configure -a & wait
 sudo DEBIAN_FRONTEND=noninteractive apt update -y & wait
 sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade --fix-missing -y & wait
