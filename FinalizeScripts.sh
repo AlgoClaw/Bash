aa2a91613cc28c97ad8efe9fdfc94cdfcabff69f
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-09-28 09:47:54 -0500

    Update FinalizeScripts.sh

--- FinalizeScripts.sh
+++ FinalizeScripts.sh
@@ -9,7 +9,6 @@ sudo chmod +x /Scripts/*.sh
 
 # Virtually Merge /Docker/Scripts/ with /usr/local/bin/ (better than symbolic links)
 sudo mergerfs -o nonempty,allow_other,use_ino "/Scripts" "/usr/local/bin" & wait
-sudo mergerfs -o nonempty,allow_other,use_ino "/Scripts/fcns" "/usr/local/bin"
 
 # Make Symbolic Links in /usr/local/bin
 #sudo ln -sf /Scripts/* /usr/local/bin
