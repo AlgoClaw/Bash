c069020aa7dd051e9b2dbc2dd256a1cf440c461e
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-09-28 09:34:43 -0500

    Update FinalizeScripts.sh

--- FinalizeScripts.sh
+++ FinalizeScripts.sh
@@ -4,14 +4,14 @@
 sudo apt install -y dos2unix mergerfs & wait
 
 # Set Permissions
-sudo dos2unix /Docker/Scripts/*.sh
-sudo chmod +x /Docker/Scripts/*.sh
+sudo dos2unix /Scripts/*.sh
+sudo chmod +x /Scripts/*.sh
 
-# Merge /Docker/Scripts/ with /usr/local/bin/
-sudo mergerfs -o nonempty,allow_other,use_ino "/Docker/Scripts" "/usr/local/bin"
+# Virtually Merge /Docker/Scripts/ with /usr/local/bin/ (better than symbolic links)
+sudo mergerfs -o nonempty,allow_other,use_ino "/Scripts" "/usr/local/bin"
 
 # Make Symbolic Links in /usr/local/bin
-#sudo ln -sf /Docker/Scripts/* /usr/local/bin
+#sudo ln -sf /Scripts/* /usr/local/bin
 
 # Delete Dead Symbolic Links in /usr/local/bin
 #sudo find /usr/local/bin -xtype l -delete
