3b8e80e18b84c75b25270fcea558d5cfad7c0168
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-09-28 08:59:47 -0500

    Update update_tmp.sh

--- update_tmp.sh
+++ update_tmp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo rm /etc/apt/apt.conf.d/20apt-esm-hook.conf & wait
-sudo apt install -y ubuntu-advantage-tools & wait
 sudo dpkg --configure -a & wait
+sudo DEBIAN_FRONTEND=noninteractive apt install -y ubuntu-advantage-tools & wait
 sudo DEBIAN_FRONTEND=noninteractive apt update -y & wait
 sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade --fix-missing -y & wait
 sudo DEBIAN_FRONTEND=noninteractive apt install -f -y & wait
