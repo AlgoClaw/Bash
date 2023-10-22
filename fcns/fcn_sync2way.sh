4aa9a098630e1512e39b67534e08114ef8429106
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-10-22 17:52:49 -0500

    Update fcn_sync2way.sh

--- fcns/fcn_sync2way.sh
+++ fcns/fcn_sync2way.sh
@@ -9,7 +9,7 @@ DIR2=${2%/}/
 sudo apt install -y rsync & wait
 
 # DIR1 to DIR2
-sudo rsync --update --times --recursive "${DIR1}./" "${DIR2}" & wait
+sudo rsync --relative --recursive --partial --progress --append-verify --update --times "${DIR1}./" "${DIR2}" & wait
 
 # DIR2 to DIR1
-sudo rsync --update --times --recursive "${DIR2}./" "${DIR1}"
+sudo rsync --relative --recursive --partial --progress --append-verify --update --times "${DIR2}./" "${DIR1}"
