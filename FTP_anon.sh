e007e66a8d49d39da0983332c18f09ca4f203179
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-11-19 18:12:14 -0600

    Create FTP_anon.sh

---
+++ FTP_anon.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Uninstall and purge FTP server installation
+sudo apt remove -y --purge vsftpd
+
+# Install FTP server
+sudo apt install -y vsftpd
+
+# Change FTP root directory to system’s root directory ("/")
+sudo usermod -d "/" ftp
+
+# Configure FTP server to allow for anonymous read/write access
+sudo rm -r "/etc/vsftpd.conf"
+sudo tee -a "/etc/vsftpd.conf" > /dev/null <<EOT
+# Default Options
+listen=YES
+local_enable=YES
+dirmessage_enable=YES
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+pam_service_name=vsftpd
+ssl_enable=NO
+#
+# Custom Options
+anonymous_enable=YES
+write_enable=YES
+anon_upload_enable=YES
+anon_mkdir_write_enable=YES
+anon_other_write_enable=YES
+dirlist_enable=YES
+force_dot_files=YES
+anon_world_readable_only=NO
+EOT
+
+# (Re)enable and (re)start FTP server
+sudo systemctl enable vsftpd.service
+sudo systemctl restart vsftpd.service
