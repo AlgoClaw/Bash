840050f544309907c40ac2900951bcc618b1b527
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-11-20 19:57:28 -0600

    Update FinalizeScripts.sh

--- FinalizeScripts.sh
+++ FinalizeScripts.sh
@@ -7,14 +7,13 @@ sudo apt install -y dos2unix mergerfs & wait
 sudo dos2unix /Scripts/*.sh & wait
 sudo chmod +x /Scripts/*.sh & wait
 
-#Unmount /usr/local/bin
-sudo umount /usr/local/bin & wait
-
 # Copy contents of /usr/local/bin to /Scripts
 sudo cp -RTn /usr/local/bin /Scripts & wait
 
-# Delete /usr/local/bin
+#Unmount /usr/local/bin
 sudo umount /usr/local/bin & wait
+
+# Delete /usr/local/bin
 sudo rm -r /usr/local/bin & wait
 
 # Make /usr/local/bin if it does not exist already
