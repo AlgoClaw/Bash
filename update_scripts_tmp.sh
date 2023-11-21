e553de3e0e2ea5c9fec0f68a4b4918c00042e731
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-11-21 00:39:59 -0600

    Update update_scripts_tmp.sh

--- update_scripts_tmp.sh
+++ update_scripts_tmp.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-sudo apt update && \
-sudo rm -f /Scripts_tmp && \
-sudo git clone https://github.com/AlgoClaw/Bash /Scripts_tmp && \
-sudo rm -r /Scripts_tmp/.git && \
-sudo cp -RT --update[=all] /Scripts_tmp/fcns /Scripts_tmp && \
-sudo rm -r /Scripts_tmp/fcns && \
-sudo cp -RT --update[=all] /Scripts_tmp /Scripts && \
-sudo rm -r /Scripts_tmp && \
-sudo umount "/usr/local/bin"  & wait
-sudo cp -RTn /usr/local/bin /Scripts && \
-sudo rm /usr/local/bin/* & wait
-sudo apt install -y dos2unix mergerfs & wait
-sudo dos2unix /Scripts/*.sh & wait
-sudo chmod +x /Scripts/*.sh & wait
+#
+sudo rm -r /Scripts_tmp > /dev/null 2>&1
+sudo git clone https://github.com/AlgoClaw/Bash /Scripts_tmp > /dev/null 2>&1
+sudo rm -r /Scripts_tmp/.git
+sudo rm -f /Scripts_tmp/README.md
+sudo cp -RTf /Scripts_tmp/fcns /Scripts_tmp
+sudo rm -r /Scripts_tmp/fcns
+sudo cp -RTf /Scripts_tmp /Scripts
+sudo rm -r /Scripts_tmp
+#
+sudo apt install -y dos2unix > /dev/null 2>&1
+sudo dos2unix /Scripts/*.sh > /dev/null 2>&1
+sudo chmod +x /Scripts/*.sh > /dev/null 2>&1
+#
+sudo umount -A "/usr/local/bin" > /dev/null 2>&1
+sudo cp -RTn /usr/local/bin /Scripts
+sudo rm /usr/local/bin/* > /dev/null 2>&1
+#
+sudo apt install -y mergerfs > /dev/null 2>&1
 sudo mergerfs -o nonempty,allow_other,use_ino "/Scripts" "/usr/local/bin"
