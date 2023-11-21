fb949dedfc82febc9a4a1dbd76738e7717d34c71
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-11-20 19:49:18 -0600

    Update FinalizeScripts.sh

--- FinalizeScripts.sh
+++ FinalizeScripts.sh
@@ -7,6 +7,9 @@ sudo apt install -y dos2unix mergerfs & wait
 sudo dos2unix /Scripts/*.sh & wait
 sudo chmod +x /Scripts/*.sh & wait
 
+#Unmount /usr/local/bin
+sudo umount /usr/local/bin & wait
+
 # Copy contents of /usr/local/bin to /Scripts
 sudo cp -RTn /usr/local/bin /Scripts & wait
 
