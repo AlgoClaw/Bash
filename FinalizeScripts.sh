ed96f1009797dbc301a71122df9fceaf8c6d184a
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-10 15:43:06 -0600

    Update FinalizeScripts.sh

--- FinalizeScripts.sh
+++ FinalizeScripts.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
 
 # Install Dependencies
-sudo apt install -y dos2unix mergerfs & wait
+sudo apt install -y dos2unix & wait
 
 # Set Permissions
-sudo dos2unix /Scripts/*.sh & wait
-sudo chmod +x /Scripts/*.sh & wait
-
-# Copy contents of /usr/local/bin to /Scripts
-sudo cp -RTn /usr/local/bin /Scripts & wait
-
-#Unmount /usr/local/bin
-sudo umount /usr/local/bin & wait
-sudo umount /usr/local/bin & wait
-
-# Delete /usr/local/bin
-#sudo rm  /usr/local/bin/* & wait
-
-# Make /usr/local/bin if it does not exist already
-#sudo mkdir -p /usr/local/bin & wait
-
-# Virtually Merge /Docker/Scripts/ with /usr/local/bin/ (better than symbolic links)
-sudo mergerfs -o nonempty,allow_other,use_ino "/Scripts" "/usr/local/bin"
-#sudo umount "/usr/local/bin"
-
-# Make Symbolic Links in /usr/local/bin
-#sudo ln -sf /Scripts/* /usr/local/bin
-
-# Delete Dead Symbolic Links in /usr/local/bin
-#sudo find /usr/local/bin -xtype l -delete
+sudo dos2unix "${0%/*}"/*.sh
+sudo chmod +x "${0%/*}"/*.sh
