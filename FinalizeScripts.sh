cdbd3f71ac0158a206bbb369e9cc5755f462934e
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-12 14:22:15 -0600

    Update FinalizeScripts.sh

--- FinalizeScripts.sh
+++ FinalizeScripts.sh
@@ -5,5 +5,5 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y dos2unix
 sudo pacman -Sy --noconfirm dos2unix --overwrite '*'
 
 # Set Permissions
-sudo dos2unix "${0%/*}"/*.sh
+sudo dos2unix "${0%/*}"/*
 sudo chmod +x "${0%/*}"/*.sh
