f510c2a2affe3aa6f811fac31866a5f2dec953e8
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-14 11:44:45 -0600

    Update fcn_sync1way_nodelete.sh

--- fcn_sync1way_nodelete.sh
+++ fcn_sync1way_nodelete.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# sudo bash fcn_sync1way_nodelete_update.sh "/path/to/source" "/path/to/destination"
+
 sudo apt install -y rsync
 
 DIR1=${1%/}/
 DIR2=${2%/}/
+
+sudo chmod -R 777 ${DIR1}
+sudo chmod -R 777 ${DIR2}
 
 sudo rsync --append-verify --partial --progress --archive --relative "${DIR1}./" "${DIR2}"
