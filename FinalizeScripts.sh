81ae22cfcd838eff5c3d00a3ca90fb5544f10b7f
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-25 23:58:19 -0600

    Update FinalizeScripts.sh

--- FinalizeScripts.sh
+++ FinalizeScripts.sh
@@ -5,5 +5,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y dos2unix
 sudo pacman -Sy --noconfirm dos2unix --overwrite '*'
 
 # Set Permissions
-sudo dos2unix "${0%/*}"/*
-sudo chmod +x "${0%/*}"/*.sh
+shopt -s globstar
+sudo dos2unix "${0%/*}"/**
+sudo chmod +x -R "${0%/*}"/*.sh
