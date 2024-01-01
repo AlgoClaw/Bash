5d96322c956d8280fbba54bf90f0ed2f8fd1bcef
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-31 20:02:51 -0600

    Delete smb_steamos.sh

--- smb_steamos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo rm /Scripts/smb_steamos_tmp.sh & wait
-
-sudo curl https://raw.githubusercontent.com/AlgoClaw/Bash/main/smb_steamos_tmp.sh --create-dirs -o /Scripts/smb_steamos_tmp.sh & wait
-
-sudo bash /Scripts/smb_steamos_tmp.sh
