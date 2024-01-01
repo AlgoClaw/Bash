91409b2ff067cb3387134fad46b85d324d60426f
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2023-12-31 18:50:20 -0600

    Update fcn_sync1way_versioning.sh

--- fcn_sync1way_versioning.sh
+++ fcn_sync1way_versioning.sh
@@ -5,8 +5,10 @@
 DIR1=${1%/}/
 DIR2=${2%/}/
 
-datetimefile=$(date +%F_%H-%M-%S)
-updates_file="updates"
+updates_file="onewaysyncupdatefile.txt"
+
+#### cleanup
+sudo rm -f "${updates_file}"
 
 #### install dependencies
 sudo DEBIAN_FRONTEND=noninteractive apt install -y rsync
@@ -30,7 +32,13 @@ sed -i 's/[^ ]* //' "${updates_file}"
 #### "${updates_file}" should now be an easily readable list of files that would be overwritten on the destination
 
 #### rename files, on the destination, that would be overwritten
-while IFS= read -r file; do mv "${DIR2}${file}" "${DIR2}${file}_${datetimefile}"; done < "${updates_file}"
+while IFS= read -r file
+do
+	unixdatetime=$(date -r "${DIR2}${file}" +%s)
+	oldfilename="${DIR2}${file}"
+newfilename="${DIR2}${file}_${unixdatetime}"
+mv "${oldfilename}" "${newfilename}"
+done < "${updates_file}"
 
 #### copy files from source to destination
 sudo rsync --append-verify --partial --archive --relative --update "${DIR1}./" "${DIR2}"
