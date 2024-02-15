a25fd7b4b9d0d44f0062e632551b094c14724b0c
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-02-15 17:02:51 -0600

    Update smb.sh

--- smb.sh
+++ smb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo rm /Scripts/smb_tmp.sh & wait
+SMBSCRDIR="/Scripts"
 
-sudo curl https://raw.githubusercontent.com/AlgoClaw/Bash/main/smb_tmp.sh --create-dirs -o /Scripts/smb_tmp.sh & wait
+sudo curl https://raw.githubusercontent.com/AlgoClaw/Bash/main/smb_tmp.sh --create-dirs -o "${SMBSCRDIR}"/smb_tmp.sh & wait
 
-sudo bash /Scripts/smb_tmp.sh
+sudo bash "${SMBSCRDIR}"/smb_tmp.sh
