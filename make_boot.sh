7c38415a8adda18d458e1dd69c4edb58ec3dedc2
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-02-19 21:27:24 -0600

    Create make_boot.sh

---
+++ make_boot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Make boot.sh script
+sudo ls "/Scripts/boot.sh" &>/dev/null || $(sudo tee -a "/Scripts/boot.sh" &>/dev/null <<EOT
+#!/bin/bash
+
+# USB Mounts
+sudo bash /Scripts/mnt_USB.sh & wait
+
+# Samba
+sudo bash /Scripts/smb.sh & wait
+
+# Start Docker
+sudo systemctl start docker & wait
+
+# LAN Mounts
+sudo bash /Scripts/mnt_LAN.sh & wait
+EOT) &>/dev/null
+
+# Add boot.sh script to crontab
+sudo crontab -l | grep -q "/Scripts/boot.sh" || $((sudo crontab -l 2>/dev/null; echo '@reboot sh "/Scripts/boot.sh"') | sudo crontab -)
