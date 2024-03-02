be3983483f3531da21774904eb5f3183436955ec
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-03-01 20:37:49 -0600

    Create fcn_sync1way_delete.sh

---
+++ fcn_sync1way_delete.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# sudo bash fcn_sync1way_delete.sh "/path/to/source" "/path/to/destination"
+
+sudo apt install -y rsync
+
+DIR1=${1%/}/
+DIR2=${2%/}/
+
+sudo chmod -R 777 ${DIR1}
+sudo chmod -R 777 ${DIR2}
+
+sudo rsync --append-verify --partial --progress --archive --relative --checksum --del "${DIR1}./" "${DIR2}"
