af3e4aa59cb0f091ccfb1a96e09d745e0fc75da8
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-03-01 22:13:14 -0600

    Update make_every_xx.sh

--- make_every_xx.sh
+++ make_every_xx.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 ########
+######## 01
+########
+
+# Make 0_Every01Min.sh script
+sudo ls "/Scripts/0_Every01Min.sh" &>/dev/null || $(sudo tee -a "/Scripts/0_Every01Min.sh" &>/dev/null <<EOT
+#!/bin/bash
+
+# sudo bash /Scripts/script.sh
+EOT) &>/dev/null
+
+# Add 0_Every01Min.sh script to crontab
+sudo crontab -l | grep -q "/Scripts/0_Every01Min.sh" || $((sudo crontab -l 2>/dev/null; echo '*/05 * * * * sh "/Scripts/0_Every01Min.sh"') | sudo crontab -)
+
+########
 ######## 05
 ########
 
