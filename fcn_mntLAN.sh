183132643f2b918f7fa3adde6144deda338ddbfc
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-05-18 16:49:09 -0500

    Update fcn_mntLAN.sh

--- fcn_mntLAN.sh
+++ fcn_mntLAN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# sudo bash "/Scripts/fcn_mntLAN.sh" "/path/to/local/mnt" "192.168.xx.xx" "/path/to/remote/dir" "username" "password" "port"
-# sudo bash "/Scripts/fcn_mntLAN.sh" "/mnt/LAN/ABC" "192.168.56.78" "/root"
+# sudo bash "/path/to/fcn_mntLAN.sh" "/path/to/local/mntpnt" "192.168.xx.xx" "/path/to/remote/dir" "username" "password" "port"
+# sudo bash "/path/to/fcn_mntLAN.sh" "/mnt/LAN/ABC" "192.168.56.78" "/root"
 
 LOCDIR="${1%/}"
 REMIPA="${2}"
@@ -13,17 +13,20 @@ PORTNO="${6:-445}"
 # Install Dependencies
 sudo apt-get install -y net-tools cifs-utils >/dev/null && \
 
+# Script Direcotry
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) >/dev/null
+
 # Check if directory exists, if not, make it.
 sudo mkdir -p "${LOCDIR}" >/dev/null
 
 # Exit if Something is mounted to LOCDIR already
-if [[ $(sudo bash "${0%/*}"/fcn_chckmnt.sh "${LOCDIR}") == 1 ]]; then
+if [[ $(sudo bash $SCRIPT_DIR/fcn_chckmnt.sh "${LOCDIR}") == 1 ]]; then
   echo "Mountpoint directory already mounted. Exiting. (${LOCDIR})"
   exit
 fi
 
 # If LOCDIR is NOT empty, lock it and exit. If empty, unlock it.
-if [[ $(sudo bash "${0%/*}"/fcn_chckdir4contents.sh "${LOCDIR}") == 1 ]]; then
+if [[ $(sudo bash $SCRIPT_DIR/fcn_chckdir4contents.sh "${LOCDIR}") == 1 ]]; then
   echo "Mountpoint directory is NOT empty. Locking directory (chattr +i). Exiting. (${LOCDIR})"
   sudo chattr +i "${LOCDIR}"
   exit
