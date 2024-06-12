9c534a0ceb76a59f313c223d4a7930bc534b81c8
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-06-12 18:17:54 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo curl https://raw.githubusercontent.com/AlgoClaw/Bash/main/update_scripts.sh --create-dirs -o "/update_scripts.sh"
+
+sudo bash "/update_scripts.sh"
+
+sudo crontab -l | grep -q "update_scripts.sh" || $((sudo crontab -l 2>/dev/null; echo '@reboot sh "/update_scripts.sh"') | sudo crontab -)
+
+sudo bash make_boot.sh
+
+sudo bash make_every_xx.sh
+
+sudo bash users_to_sudoers.sh
+
+sudo bash users_to_hosts.sh
