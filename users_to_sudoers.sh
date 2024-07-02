b1a1cc7b967d9169a4b6708a4ad35291ff7fbe6b
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-07-01 22:16:57 -0500

    Update users_to_sudoers.sh

--- users_to_sudoers.sh
+++ users_to_sudoers.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 ### Adds $USER to "/etc/sudoers"
-### When executing a "sudo" command, $USER is passed to the variable $SUDO_USER
+### When executing a "sudo" command, $USER is passed to the variable $SUDO_USER (not always true)
 
+if [$(sudo echo $SUDO_USER) -eq ""]
+then
+else
 sudo cat "/etc/sudoers" | grep -q "$SUDO_USER ALL = (ALL) NOPASSWD: ALL" ||  $(sudo tee -a "/etc/sudoers" > /dev/null <<EOT
-
 $SUDO_USER ALL = (ALL) NOPASSWD: ALL
 EOT)
+fi
 
 ### Adds "root" to "/etc/sudoers"
-### When executing a "sudo" command, $USER changes to "root"
+### When executing a "sudo" command, $USER changes to "root" (not always true)
 
+if [$(sudo echo $USER) -eq ""]
+then
+else
 sudo cat "/etc/sudoers" | grep -q "$USER ALL = (ALL) NOPASSWD: ALL" ||  $(sudo tee -a "/etc/sudoers" > /dev/null <<EOT
-
 $USER ALL = (ALL) NOPASSWD: ALL
 EOT)
+fi
