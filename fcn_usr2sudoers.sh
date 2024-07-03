fe918e2bf1e1c57cc6ca94630209c5e0e5ae0978
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-07-02 22:06:11 -0500

    Update fcn_usr2sudoers.sh

--- fcn_usr2sudoers.sh
+++ fcn_usr2sudoers.sh
@@ -3,11 +3,13 @@
 USRSTR="${1}"
 SUDOLOC="/etc/sudoers"
 
+
 if [ "${USRSTR}" = "" ]; then
 	user_empty=true
 else
 	user_empty=false
 fi
+
 
 str1=$(sudo grep "$USRSTR	ALL=(ALL:ALL) NOPASSWD:ALL" ${SUDOLOC} | tail -1)
 str2=$(echo "$USRSTR	ALL=(ALL:ALL) NOPASSWD:ALL")
@@ -17,11 +19,11 @@ else
 	user_entryexist=false
 fi
 
+
 if [ "${user_empty}" = false ] && [ "${user_entryexist}" = false ]; then
 	echo "${USRSTR}	ALL=(ALL:ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
 fi
 
-#sudo $(awk '!a[$0]++' ${SUDOLOC} > ${SUDOLOC}1)
 
 sudo apt install -y dos2unix &>/dev/null
 sudo dos2unix ${SUDOLOC} &>/dev/null
