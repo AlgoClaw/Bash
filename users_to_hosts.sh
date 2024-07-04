000311485004041fce690e17daa0780d1fd91de2
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-07-04 12:54:26 -0500

    Update users_to_hosts.sh

--- users_to_hosts.sh
+++ users_to_hosts.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 
-###
-### Add "$SUDO_USER" to "/etc/hosts"
+FILE="/etc/hosts"
 
-sudo cat "/etc/hosts" | grep -q "127.0.0.1 $SUDO_USER" ||  $(sudo tee -a "/etc/hosts" > /dev/null <<EOT
-
-127.0.0.1 $SUDO_USER
-EOT)
-
-###
-### Adds "$USER" to "/etc/hosts"
-
-sudo cat "/etc/hosts" | grep -q "127.0.0.1 $USER" ||  $(sudo tee -a "/etc/hosts" > /dev/null <<EOT
-
-127.0.0.1 $USER
-EOT)
-
-###
-### Add "$HOSTNAME" to "/etc/hosts"
-
-sudo cat "/etc/hosts" | grep -q "127.0.0.1 $HOSTNAME" ||  $(sudo tee -a "/etc/hosts" > /dev/null <<EOT
-
-127.0.0.1 $HOSTNAME
-EOT)
-
-###
-### Add "localhost" to "/etc/hosts"
-
-sudo cat "/etc/hosts" | grep -q "127.0.0.1 localhost" ||  $(sudo tee -a "/etc/hosts" > /dev/null <<EOT
-
-127.0.0.1 localhost
-EOT)
+sudo bash fcn_str2file.sh "${FILE}" "127.0.0.1 localhost"
+sudo bash fcn_str2file.sh "${FILE}" "127.0.0.1 root"
+sudo bash fcn_str2file.sh "${FILE}" "127.0.0.1 ${USER}"
+sudo bash fcn_str2file.sh "${FILE}" "127.0.0.1 ${HOSTNAME}"
