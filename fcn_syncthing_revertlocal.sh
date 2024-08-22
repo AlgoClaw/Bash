93a247fde7b36fd081c6efb689a69c6f9d6decf5
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-08-21 21:04:15 -0500

    Update fcn_syncthing_revertlocal.sh

--- fcn_syncthing_revertlocal.sh
+++ fcn_syncthing_revertlocal.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# sudo bash "/Scripts/fcn_syncthing_revertlocal.sh" "8384" "abcdefghijk1234567890" "dirID"
+# sudo bash "/Scripts/fcn_SyncThing_RevertLocal.sh" "abcdefghijk1234567890" "dirID"
+# sudo bash "/Scripts/fcn_SyncThing_RevertLocal.sh" "abcdefghijk1234567890" "dirID" "8384"
 
-host=https://localhost:"${1}"
-apikey="${2}"
-folder="${3}"
+apikey="${1}"
+folder="${2}"
+host=https://127.0.0.1:"${3:-8384}"
 
 if ! curl -skH "X-API-Key: $apikey" "$host/rest/db/status?folder=$folder" | grep -q 'receiveOnlyChanged.*": [1-9]' ; then
 exit 0
