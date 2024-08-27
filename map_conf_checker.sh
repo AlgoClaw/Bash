b5580cedd10945322ed02e8108c8aa27e1a77fe6
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-08-26 19:36:20 -0500

    Add files via upload

---
+++ map_conf_checker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# sudo bash /Scripts/map_conf_checker.sh
+
+file="/Docker/SWAG/nginx/map.conf" 
+
+###
+
+# Define "copy" filename (appending "_copy" to $file)
+copy="${file%.*}_copy.${file##*.}"
+
+# If the files are the same, exit.
+sudo cmp --silent ${file} ${copy} && exit || \
+
+# If the files are different, do the following:
+# Copy $file to $copy, including overwriting.
+sudo cp ${file} ${copy}
+
+# Restart nginx
+sudo docker exec SWAG nginx -s reload
