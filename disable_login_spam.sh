b0b5bbcdad84140512a19fefdd2423bfc7be31e1
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2024-09-14 15:17:46 -0500

    Update disable_login_spam.sh

--- disable_login_spam.sh
+++ disable_login_spam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-touch ~/.hushlogin # this hides all login messages
+# Hide System-Wide Messages Which Display for All Users
+touch ~/.hushlogin 
 
-sudo pro config set apt_news=false
-
-sudo systemctl mask apt-news.service
-sudo systemctl mask esm-cache.service
+# Disable RPi User-Specific Warnings
+sudo rm /etc/profile.d/sshpwd.sh
+sudo rm /etc/profile.d/wifi-check.sh
