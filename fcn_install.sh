6a04b0e16663e3f1a02c113c6f465cccff0d0fc6
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2025-02-02 12:27:40 -0600

    Add files via upload

---
+++ fcn_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# sudo bash fcn_install.sh "rsync"
+
+PKG=${1%}
+
+INSTALL_STATE=$(dpkg -l | grep "${PKG}" | grep ii >/dev/null && echo "1" || echo "0")
+
+#echo "${INSTALL_STATE}"
+
+# 0 = not installed
+# 1 = installed
+
+if [[ "${INSTALL_STATE}" == 1 ]]; then
+  echo '"'"${PKG}"'"'" already installed"
+else
+  echo '"'"${PKG}"'"'" not installed, installing now"
+  sudo apt install -y ${PKG}
+fi
