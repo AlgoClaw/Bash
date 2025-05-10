2181d27827280f1c972435ceec26cfd178e7d022
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2025-05-10 06:14:45 -0500

    Update fcn_install.sh

--- fcn_install.sh
+++ fcn_install.sh
@@ -11,9 +11,7 @@ INSTALL_STATE=$(dpkg -l | grep "${PKG}" | grep ii >/dev/null && echo "1" || echo
 # 0 = not installed
 # 1 = installed
 
-if [[ "${INSTALL_STATE}" == 1 ]]; then
-  #echo '"'"${PKG}"'"'" already installed"
-else
+if [[ "${INSTALL_STATE}" == 0 ]]; then
   #echo '"'"${PKG}"'"'" not installed, installing now"
   sudo DEBIAN_FRONTEND=noninteractive apt install -y ${PKG}
 fi
