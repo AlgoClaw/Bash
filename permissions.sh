07fc8a8fa097ce3de644335d8735e558e5e157a9
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2025-06-19 16:09:02 -0500

    Create permissions.sh

---
+++ permissions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo chown -R 1000:1000 /tmp/
+sudo chmod -R 777 /tmp/
+
+sudo chown -R 1000:1000 /docker/
+sudo chmod -R 777 /docker/
+
+sudo chown -R 1000:1000 /scripts/
+sudo chmod -R 777 /scripts/
