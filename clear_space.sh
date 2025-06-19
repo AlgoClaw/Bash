d7659f3dc810d62345bd7989d0d1b8cdf2f9fb14
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2025-06-19 16:00:23 -0500

    Update clear_space.sh

--- clear_space.sh
+++ clear_space.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sudo rm -r /tmp
-sudo rm -r /boot.bak
-sudo rm -r /var/tmp
-sudo docker system prune -f
-sudo rm -r /docker/Unifi/data/db/_tmp*
+sudo rm -rf /tmp >/dev/null 2>&1
+sudo rm -rf /boot.bak >/dev/null 2>&1
+sudo rm -rf /var/tmp >/dev/null 2>&1
+sudo docker system prune -f >/dev/null 2>&1
+sudo rm -f /docker/Unifi/data/db/_tmp* >/dev/null 2>&1
+sudo rm -f /var/log/auth.log.* >/dev/null 2>&1
+sudo rm -f /var/mail/mail >/dev/null 2>&1
+
+sudo mkdir -p /tmp >/dev/null 2>&1
+sudo chown -R 1000:1000 /tmp/ >/dev/null 2>&1
+sudo chmod -R 777 /tmp/ >/dev/null 2>&1
