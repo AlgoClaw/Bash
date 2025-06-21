64c0cb78f309ba294288b1e891a7e8dfd15183d7
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2025-06-21 15:11:40 -0500

    Update fcn_virdir.sh

--- fcn_virdir.sh
+++ fcn_virdir.sh
@@ -9,30 +9,28 @@ SRC="${1}"
 DST="${2}"
 
 if [ "${SRC}" == "" ]; then
-	exit
+   exit
 fi
 
 if [ "${DST}" == "" ]; then
     exit
 fi
 
-SRC="${SRC%/}/"
-DST="${DST%/}/"
+SRC="${SRC%/}"
+DST="${DST%/}"
 
-# Script Direcotry
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) >/dev/null 2>&1
-
-# Forcefully Unmount Directory
+# Forcefully Unmount DST
 sudo umount -fl ${DST} >/dev/null 2>&1
 
-# Make Directory
-sudo mkdir -p ${DST}
+# Make Directories
+sudo mkdir -p "${SRC}"
+sudo mkdir -p "${DST}"
 
 # If DST is NOT empty, exit.
-if [[ $(sudo bash $SCRIPT_DIR/fcn_chckdir4contents.sh ${DST}) == 1 ]]; then
+if [[ $(sudo bash /scripts/fcn_chckdir4contents.sh "${DST}") == 1 ]]; then
     echo "Mountpoint is NOT empty. Exiting."
     exit
 fi
 
 # "Merge" SRC to DST
-sudo mergerfs -o nonempty,allow_other,use_ino,category.create=ff ${SRC} ${DST}
+sudo mergerfs -o nonempty,allow_other,use_ino,category.create=ff "${SRC}" "${DST}"
