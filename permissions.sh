81418ef9f6323f5741ef7ee85cf2e02ebae2d3a3
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2025-07-10 10:44:14 -0500

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -3,19 +3,19 @@
 # Single Instance Lock
 exec 9>"/tmp/$(basename "$0").lock" && flock -n 9 || { echo "Script is already running. Exiting."; exit 1; }
 
-sudo chown -R 1000:1000 /tmp/
-sudo chmod -R 777 /tmp/
+sudo chown -R 1000:1000 /tmp/ >/dev/null 2>&1
+sudo chmod -R 777 /tmp/ >/dev/null 2>&1
 
-sudo chown -R 1000:1000 /docker/
-sudo chmod -R 777 /docker/
+sudo chown -R 1000:1000 /docker/ >/dev/null 2>&1
+sudo chmod -R 777 /docker/ >/dev/null 2>&1
 
-sudo chown -R 1000:1000 /scripts/
-sudo chmod -R 777 /scripts/
+sudo chown -R 1000:1000 /scripts/ >/dev/null 2>&1
+sudo chmod -R 777 /scripts/ >/dev/null 2>&1
 
 # Future Removal
 
-sudo chown -R 1000:1000 /Docker/
-sudo chmod -R 777 /Docker/
+sudo chown -R 1000:1000 /Docker/ >/dev/null 2>&1
+sudo chmod -R 777 /Docker/ >/dev/null 2>&1
 
-sudo chown -R 1000:1000 /Scripts/
-sudo chmod -R 777 /Scripts/
+sudo chown -R 1000:1000 /Scripts/ >/dev/null 2>&1
+sudo chmod -R 777 /Scripts/ >/dev/null 2>&1
