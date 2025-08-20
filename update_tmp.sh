98257999413f049368d81aaa0a3d7154595a30da
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2025-08-19 22:12:36 -0500

    Update update_tmp.sh

--- update_tmp.sh
+++ update_tmp.sh
@@ -2,7 +2,7 @@
 
 sudo rm -r /etc/apt/apt.conf.d/*esm* >/dev/null & wait
 sudo dpkg --configure -a & wait
-sudo DEBIAN_FRONTEND=noninteractive apt install -y ubuntu-advantage-tools & wait
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-advantage-tools & wait
 sudo DEBIAN_FRONTEND=noninteractive apt update -y & wait
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y & wait
 sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade --fix-missing -y & wait
