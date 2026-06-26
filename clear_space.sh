440f29d8ba690d30b9c05e3608da5e1c7831e12e
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2026-06-26 10:20:52 -0500

    Refactor clear_space.sh to improve file removal

--- clear_space.sh
+++ clear_space.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-sudo rm -rf /tmp >/dev/null 2>&1
-sudo rm -rf /boot.bak >/dev/null 2>&1
-sudo rm -rf /var/tmp >/dev/null 2>&1
-sudo docker system prune -f >/dev/null 2>&1
-sudo rm -f /docker/Unifi/data/db/_tmp* >/dev/null 2>&1
-sudo rm -f /var/log/auth.log* >/dev/null 2>&1
-sudo rm -f /var/mail/mail >/dev/null 2>&1
+sudo rm -rf "/tmp" >/dev/null 2>&1
+sudo rm -rf "/boot.bak" >/dev/null 2>&1
+sudo rm -rf "/var/tmp" >/dev/null 2>&1
+sudo rm -f "/var/log/auth.log*" >/dev/null 2>&1
+sudo rm -f "/var/mail/mail" >/dev/null 2>&1
+sudo rm -f "/.deleted" >/dev/null 2>&1
+sudo rm -f "/docker/Unifi/data/db/_tmp*" >/dev/null 2>&1
 
-sudo mkdir -p /tmp >/dev/null 2>&1
-sudo chown -R 1000:1000 /tmp/ >/dev/null 2>&1
-sudo chmod -R 777 /tmp/ >/dev/null 2>&1
+sudo docker system prune -f >/dev/null 2>&1
+
+# Remake /tmp
+sudo mkdir -p "/tmp" >/dev/null 2>&1
+sudo chown -R 1000:1000 "/tmp/" >/dev/null 2>&1
+sudo chmod -R 777 "/tmp/" >/dev/null 2>&1
