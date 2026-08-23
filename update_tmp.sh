276c2fd041f8e8e1dd5fc80839fcefe902f4a92d
AlgoClaw <139091238+AlgoClaw@users.noreply.github.com>
2026-08-23 09:14:03 -0500

    Update update_tmp.sh

--- update_tmp.sh
+++ update_tmp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo rm -r /etc/apt/apt.conf.d/*esm* >/dev/null & wait
-sudo dpkg --configure -a & wait
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-advantage-tools & wait
+sudo dpkg --force-confdef --force-confold --configure -a & wait
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" ubuntu-advantage-tools & wait
 sudo DEBIAN_FRONTEND=noninteractive apt update -y & wait
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y & wait
-sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade --fix-missing -y & wait
-sudo DEBIAN_FRONTEND=noninteractive apt install -f -y & wait
+sudo DEBIAN_FRONTEND=noninteractive apt full-upgrade --fix-missing -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" & wait
+sudo DEBIAN_FRONTEND=noninteractive apt install -f -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" & wait
 sudo DEBIAN_FRONTEND=noninteractive apt autoremove -y & wait
